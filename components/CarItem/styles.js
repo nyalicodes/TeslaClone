4b9b7bde04bf576c417bfa4547c0633177b07798
Nyaladzi Tjibha <tjibhanyaladzi@gmail.com>
2021-01-23 03:33:37 +0200

    Car Item component

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -5,29 +5,34 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
     },
-    
     titles: {
         marginTop: '30%',
         width: '100%',
         alignItems: 'center'
     },
-    
     title: {
         fontSize: 40,
         fontWeight: '500',
     },
-    
     subtitle: {
         fontSize: 16,
         color: '#5c5e62'
     },
-    
+    subtitleCTA: {
+        textDecorationLine: 'underline',
+        
+    },
     image: {
         width: '100%',
         height: '100%',
         resizeMode: 'cover',
         position: 'absolute'
-    }
+    },
+    buttonsContainer: {
+        position: 'absolute',
+        bottom: 50,
+        width: '100%'
+    },
 })
 
 export default styles;
